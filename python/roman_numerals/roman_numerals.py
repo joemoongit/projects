e5d428918c2123b4c4b6d280583eed33031217bc
joemoon <joemoon817@gmail.com>
2019-01-17 14:00:38 -0800

    Fourth commit, updated names & protection levels

--- python/roman_numerals/roman_numerals.py
+++ python/roman_numerals/roman_numerals.py
@@ -6,7 +6,7 @@ class RomanNumeralizer:
     def __init__(self, dictionary):
         self.dictionary = dictionary
 
-    def correct_nines(self, string):
+    def _correct_nines(self, string):
         string = list(string)
         for i in range(len(string)-2):
             if string[i] == string[i+2] != string[i+1]:
@@ -14,29 +14,29 @@ class RomanNumeralizer:
                 string[i+2] = list(self.dictionary.keys())[list(self.dictionary.values()).index(self.dictionary[string[i+2]]*2)]
         return ''.join(string)
 
-    def assign_roman_numerals(self):
+    def _assign_roman_numerals(self):
         index, list_of_keys = 0, []
         for key, value in self.dictionary.items():
             list_of_keys += key
-            if self.number//value > 0 and self.number//value < 4:
-                for i in range(self.number//value):
-                    self.letter += key
-                self.number = self.number % value
-            elif self.number//value == 4:           # correct 4's
-                self.letter += key
-                self.letter += list_of_keys[index-1]
-                self.number = self.number % value
+            if self._int//value > 0 and self._int//value < 4:
+                for i in range(self._int//value):
+                    self._characaters += key
+                self._int = self._int % value
+            elif self._int//value == 4:           # correct 4's
+                self._characaters += key
+                self._characaters += list_of_keys[index-1]
+                self._int = self._int % value
             index += 1
-        return self.letter
+        return self._characaters
 
-    def return_roman_numeral(self, number):
-        self.number = number
-        self.letter = ''
-        return self.correct_nines(self.assign_roman_numerals())
+    def convert_int_to_roman_numeral_characters(self, _int):
+        self._int = _int
+        self._characaters = ''
+        return self._correct_nines(self._assign_roman_numerals())
 
 if __name__ == "__main__":
 
     Dictionary = {"M":1000, "D":500, "C":100, "L":50, "X":10, "V":5, "I":1}
     
     r = RomanNumeralizer(Dictionary)
-    print(r.return_roman_numeral(2019))
+    print(r.convert_int_to_roman_numeral_characters(2019))
--- python/roman_numerals/roman_numerals_test.py
+++ python/roman_numerals/roman_numerals_test.py
@@ -3,15 +3,16 @@ from roman_numerals import RomanNumeralizer
 
 class TestRomanNumeralizer(unittest.TestCase):
     """
-    Tests for Roman Numeralizer (ignore the input, please enter any integer)
+    Tests for Roman Numeralizer
     """
 
     def test(self):
         Dictionary = {"M":1000, "D":500, "C":100, "L":50, "X":10, "V":5, "I":1}
         self.r = RomanNumeralizer(Dictionary)
-        assert self.r.return_roman_numeral(1999) == "MCMXCIX"
-        assert self.r.return_roman_numeral(2000) != "MCMXCIX"
-        assert self.r.return_roman_numeral(2019) == "MMXIX"
+        assert self.r.convert_int_to_roman_numeral_characters(1999) == "MCMXCIX"
+        assert self.r.convert_int_to_roman_numeral_characters(2000) != "MCMXCIX"
+        assert self.r.convert_int_to_roman_numeral_characters(2019) == "MMXIX"
+        assert self.r.convert_int_to_roman_numeral_characters(1976) == "MCMLXXVI"
 
 
 if __name__ == "__main__":
